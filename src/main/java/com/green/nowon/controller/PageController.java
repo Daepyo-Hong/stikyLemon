abf3e0fc4466c943f2ad5f031aa7a3a150b88b19
Administrator <Administrator@304-26>
2022-10-31 16:29:49 +0900

    1031-default

---
+++ src/main/java/com/green/nowon/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.green.nowon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+	
+	@GetMapping("/signin")
+	public String signin() {
+		return "views/sign/signin";
+	}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		return "views/sign/signup";
+	}
+	
+}
