1f162175bb94fee92ae028adbff56e7edaaa8863
jun <jun@218.38.137.27>
2022-11-27 22:24:14 +0900

    1127 90%완

--- src/main/java/com/green/nowon/controller/PageController.java
+++ src/main/java/com/green/nowon/controller/PageController.java
@@ -5,15 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-	
+
 	@GetMapping("/signin")
 	public String signin() {
 		return "views/sign/signin";
 	}
-	
+
+	@GetMapping("/logup")
+	public String logup() {
+		return "views/sign/signin2";
+	}
+
 	@GetMapping("/signup")
 	public String signup() {
 		return "views/sign/signup";
 	}
-	
+
+	@GetMapping("/login")
+	public String login() {
+		return "views/sign/signup2";
+	}
 }
