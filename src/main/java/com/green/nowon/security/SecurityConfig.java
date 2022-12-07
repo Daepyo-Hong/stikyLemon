97f4f5f69349a7a1046cad032b5d291315ec008f
DP <tree1787@naver.com>
2022-12-07 16:40:32 +0900

    shop 틀 완료

---
+++ src/main/java/com/green/nowon/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.green.nowon.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+
+                        .antMatchers("/**").permitAll()
+                        //.antMatchers("/admin/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin()
+
+        ;
+        return http.build();
+    }
+}
