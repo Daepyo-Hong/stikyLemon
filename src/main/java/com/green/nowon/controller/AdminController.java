9174ff3e39e5e87a46a21aa15923aef9535161bb
DP <tree1787@naver.com>
2022-12-12 17:19:19 +0900

    12월12일 17:18 관리자단 상품리스트 레이아웃만. 관리자단 상품디테일 할 예정

--- src/main/java/com/green/nowon/controller/AdminController.java
+++ src/main/java/com/green/nowon/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.green.nowon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
@@ -18,5 +20,11 @@ public class AdminController {
     @GetMapping("/admin/goods/list")
     public String adminGoodslist() {
         return "adminpage/goods/list";
+    }
+
+    @GetMapping("/admin/goods/{ino}")
+    public String goodsupdate(@PathVariable long ino){
+        //서비스처리해서 상세내용 모델에 담아서 디테일페이지로 가져갈듯?
+        return "adminpage/goods/detail";
     }
 }
