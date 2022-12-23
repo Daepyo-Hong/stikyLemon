ada0471e204efdd6911f84b7b0229bdac1d3977f
DP <tree1787@naver.com>
2022-12-23 11:42:13 +0900

    g

--- src/main/java/com/green/nowon/controller/PageController.java
+++ src/main/java/com/green/nowon/controller/PageController.java
@@ -1,5 +1,8 @@
 package com.green.nowon.controller;
 
+import com.green.nowon.domain.entity.GoodsEntityRepository;
+import com.green.nowon.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,34 +10,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-	@GetMapping("/comm/goods/list")
-	public String shop(){
-		return "views/main/shop";
-	}
+    @Autowired
+    GoodsService goodsService;
+    //shop 버튼 눌렀을 때
+    @GetMapping("/comm/goods/list")
+    public String shop(Model model) {
+        //goodsService.findAllGoods(model);
+        return "views/main/shop";
+    }
 
-	//장바구니로 이동
-	@GetMapping("/members/cart")
-	public String cart(Model model){
-		//임시 list 저장해놓은것
-		model.addAttribute("list","value");
-		return "views/user/cart";
-	}
-	@GetMapping("/faq-list")
-	public String faqList() {
-		return "views/admin/faq/faq-list";
-	}
+    //장바구니로 이동
+    @GetMapping("/members/cart")
+    public String cart(Model model) {
+        //임시 list 저장해놓은것
+        model.addAttribute("list", "value");
+        return "views/user/cart";
+    }
 
-	@GetMapping("/comm/goods/detail") // goods gno{num}으로 수정 예정.
-	public String goods() {
-		return "views/goods/goodsDetailPage";
-	}
+    @GetMapping("/faq-list")
+    public String faqList() {
+        return "views/admin/faq/faq-list";
+    }
 
-	@GetMapping("/comm/contact") // goods gno{num}으로 수정 예정.
-	public String contact() {
-		return "views/main/contact";
-	}
+    @GetMapping("/comm/goods/detail") // goods gno{num}으로 수정 예정.
+    public String goods() {
+        return "views/goods/goodsDetailPage";
+    }
 
-
+    @GetMapping("/comm/contact") // goods gno{num}으로 수정 예정.
+    public String contact() {
+        return "views/main/contact";
+    }
 
 
 }
