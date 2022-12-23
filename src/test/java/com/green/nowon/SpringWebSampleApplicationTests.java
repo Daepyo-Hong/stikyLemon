d1eb139295d0be4f6c4867b335d593526f621698
youngjun <yj0913k@naver.com>
2022-12-23 11:50:26 +0900

    1222 별점 추가.(상품이미지 등록 안됨.)

--- src/test/java/com/green/nowon/SpringWebSampleApplicationTests.java
+++ src/test/java/com/green/nowon/SpringWebSampleApplicationTests.java
@@ -19,38 +19,39 @@ import javax.transaction.Transactional;
 class SpringWebSampleApplicationTests {
 
 
-	//멤버엔티티에 접근할 멤레포
-	@Autowired
-	MemberEntityRepository mrepo;
-	//패스워드인코딩
-	@Autowired
-	PasswordEncoder pe;
+    //멤버엔티티에 접근할 멤레포
+    @Autowired
+    MemberEntityRepository mrepo;
+    //패스워드인코딩
+    @Autowired
+    PasswordEncoder pe;
 
-	@Test
-	void 회원가입이잘되는지() {
-		mrepo.save(MemberEntity.builder()
-				.name("관리자")
-				.email("admin@test.com")
-				.pass(pe.encode("1234"))
-				.build()
-				.addRole(MyRole.USER)
-				.addRole(MyRole.ADMIN)
-		);
-	}
-	@Autowired
-	DestinationEntityRepository destRepo;
+    //@Test
+    void 회원가입이잘되는지() {
+        mrepo.save(MemberEntity.builder()
+                .name("관리자")
+                .email("admin@test.com")
+                .pass(pe.encode("1234"))
+                .build()
+                .addRole(MyRole.USER)
+                .addRole(MyRole.ADMIN)
+        );
+    }
 
-	@Commit
-	@Transactional
-	@Test
-	void 배송지입력테스트() {
-		mrepo.findById(1L).orElseThrow()
-				.addAddress(DestinationEntity.builder()
-						.zonecode("54321")
-						.address("주소임니둥3")
-						.build());
+    @Autowired
+    DestinationEntityRepository destRepo;
 
-	}
+    @Commit
+    @Transactional
+    //@Test
+    void 배송지입력테스트() {
+        mrepo.findById(1L).orElseThrow()
+                .addAddress(DestinationEntity.builder()
+                        .zonecode("54321")
+                        .address("주소임니둥3")
+                        .build());
 
-	}
+    }
+
 }
+
